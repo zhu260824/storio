164e334ac0e508989bfb6a8766a399c71e34d5e2
Dmitrii Nikitin <nikitin.da.90@gmail.com>
2016-05-28 02:12:36 +0300

    Dammy table notification test

--- storio-sqlite/src/test/java/com/pushtorefresh/storio/sqlite/integration/ObserveChangesTest.java
+++ storio-sqlite/src/test/java/com/pushtorefresh/storio/sqlite/integration/ObserveChangesTest.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 
 import com.pushtorefresh.storio.sqlite.BuildConfig;
 import com.pushtorefresh.storio.sqlite.Changes;
+import com.pushtorefresh.storio.sqlite.queries.RawQuery;
 import com.pushtorefresh.storio.test.AbstractEmissionChecker;
 
 import org.junit.Test;
@@ -105,6 +106,46 @@ public class ObserveChangesTest extends BaseTest {
         deleteUsersBlocking(users);
 
         // Should receive changes of Users table
+        emissionChecker.awaitNextExpectedValue();
+
+        emissionChecker.assertThatNoExpectedValuesLeft();
+
+        subscription.unsubscribe();
+    }
+
+    @Test
+    public void dummyTableEmission() {
+        final String dummyTable = "dummy_table";    // There is no existing table in db
+        final Queue<Changes> expectedChanges = new LinkedList<Changes>();
+        expectedChanges.add(Changes.newInstance(dummyTable));
+
+        final AbstractEmissionChecker emissionChecker = new AbstractEmissionChecker<Changes>(expectedChanges) {
+            @NonNull
+            @Override
+            public Subscription subscribe() {
+                return storIOSQLite
+                        .observeChangesInTable(dummyTable)
+                        .subscribe(new Action1<Changes>() {
+                            @Override
+                            public void call(Changes changes) {
+                                onNextObtained(changes);
+                            }
+                        });
+            }
+        };
+
+        final Subscription subscription = emissionChecker.subscribe();
+
+        storIOSQLite
+                .executeSQL()
+                .withQuery(RawQuery.builder()
+                        .query("delete from " + UserTableMeta.TABLE)
+                        .affectsTables(dummyTable)  // Notify about dummy table changes
+                        .build())
+                .prepare()
+                .executeAsBlocking();
+
+        // Should receive changes of dummy table
         emissionChecker.awaitNextExpectedValue();
 
         emissionChecker.assertThatNoExpectedValuesLeft();
